aad2811b99cff6775b2dd499b3d8e736be6244ff
JayVae <919433524@qq.com>
2021-03-12 15:37:26 +0800

    DI

--- src/main/java/com/geely/design/principle/dependenceinversion/Test.java
+++ src/main/java/com/geely/design/principle/dependenceinversion/Test.java
@@ -1,7 +1,11 @@
 package com.geely.design.principle.dependenceinversion;
 
 /**
- * Created by geely
+ * Created by JS
+ * 在这个案例中，test是高层，geely是其低层，增加时不应该依赖修改geely，而应该依赖其抽象。
+ * v1版本中，直接修改geely，新增课程时需要在其中增加代码；
+ * v2版本中，将课程进行抽象，新增的课程实现接口，并传给geely，geely中代码不变
+ * v3中，增加了可以注入
  */
 public class Test {
 
