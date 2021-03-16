d3f25ff57db39604236ea7cd668f24764071b22c
JayVae <919433524@qq.com>
2021-03-16 21:39:58 +0800

    mediator

--- src/main/java/com/geely/design/pattern/creational/builder/Test.java
+++ src/main/java/com/geely/design/pattern/creational/builder/Test.java
@@ -1,7 +1,9 @@
 package com.geely.design.pattern.creational.builder;
 
 /**
- * Created by geely
+ * Created by JS
+ * 建造者（Builder）模式在应用过程中可以根据需要改变，如果创建的产品种类只有一种，只需要一个具体建造者，
+ * 这时可以省略掉抽象建造者，甚至可以省略掉指挥者角色。
  */
 public class Test {
     public static void main(String[] args) {
